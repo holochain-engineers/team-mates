5210d26b611141c8fb31ef6f295647aa7516c42c
Thomas Cal <nphias@gmail>
2022-01-15 21:09:38 +0000

    environment fix

--- ui/src/environments/environment.player2.ts
+++ ui/src/environments/environment.player2.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   mock: false,
   HOST_URL:'ws://127.0.0.1:8889',
-  APP_ID:'test-app'
+  APP_ID:'team-mates'
 };
 
 /*
--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   mock: false,
   HOST_URL: 'ws://127.0.0.1:8889',
   //HOST_URL:'ws://192.168.1.100:8889',
-  APP_ID:'test-app'
+  APP_ID:'team-mates'
 };
