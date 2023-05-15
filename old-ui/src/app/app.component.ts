47e07446f4fbeb83879b39679c8745e23e5e2ea8
Thomas <nphias@gmail.com>
2023-05-15 17:24:18 +0100

    remove rubbish2

--- old-ui/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `<component-container></component-container>`
-})
-export class AppComponent {
-  title = 'team-mates';
-}
